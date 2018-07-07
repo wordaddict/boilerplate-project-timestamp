7c0f880635719564bec1de07c02e4134b5f3a75b
Adeyinka Micheal <madeyinka6@gmail.com>
2018-07-07 18:23:18 +0100

    completed the project

--- server.js
+++ server.js
@@ -24,9 +24,31 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// major entry point
+app.get("/api/timestamp/:date_string?", (req, res) => {
+  let date_string = req.params.date_string;
+  let unix;
+  let utc;
+  if (date_string === undefined) {
+    unix = new Date().getTime();
+    utc = new Date().toUTCString();
+    return res.send({
+      "unix": unix,
+      "utc": utc,
+    })
+  }
+
+  unix = new Date(date_string).getTime();
+  utc = new Date(date_string).toUTCString();
+  return res.send({
+    "unix": unix,
+    "utc": utc,
+  })
+})
+
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
